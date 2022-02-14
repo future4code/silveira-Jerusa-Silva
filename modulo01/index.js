8cf884e1cabbefaa21239846142661fd858ab170
Simone Xavier <jerusa.xaviers@gmail.com>
2022-02-14 17:46:40 -0300

    Exercicio correto

--- modulo01/index.js
+++ modulo01/index.js
@@ -52,15 +52,18 @@
 
      // Questão 3.    /
 
-     // Incompleto /
+     
 
      let a = 10;
      let b = 25;
-     let c = undefined;
+     let c =;
 
      c=a;
      a=b;
      b=c;
+
+     console.log("o novo valor de a é",a);
+     console.log("o novo valor de b é",b);
 
      
 
