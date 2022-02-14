33d8d4b57881f2e9e5ec5eb38c7c6efebbafe8ce
Simone Xavier <jerusa.xaviers@gmail.com>
2022-02-14 17:07:49 -0300

    exercicios

---
+++ modulo01/index.js
@@ -0,0 +1,77 @@
+
+/**                           Respostas dos exercícios de interpretação de código.                       
+
+ Questão 1: 
+ Inicialmente o resultado é "10 10", mas como houve a modificação do valor atribuido a letra "b"
+  o resultado final é "10 5". 
+
+ Questão 2:
+  Não houve uma atribuição ao valor da letra "c", por isso, automaticamente ocorre a divisão
+ * por isso o valor impresso é "10 10 10". 
+
+  Questão 3:
+ *  Nome para as variáveis: horaTrabalhadaPorDia e valorRecebidoPorDia o objetivo é identificar
+ * o quanto vale a hora trabalhada. */
+
+
+  
+  //                           Respostas dos exercícios de escrita de códigos.         /
+
+
+
+     // Questão 1.    /
+
+   
+
+     /** Criei variaveis "let" para nome e idade, como não houve atribuição de valores
+      * foi impresso "undefined", mas a pasta existe e a qualquer momento pode ser atribuídos
+      * valores. */
+
+
+     /** Foram impressos dois tipos de variáveis: String e Number.  */
+
+     const saudacao = "Oi";
+     const nome = "Jerusa";
+     const corpo = "você tem";
+     const idade = 33;
+     const fim = "anos";
+
+     console.log(saudacao,nome,corpo,idade,fim);
+
+
+     // Questão 2.     /
+
+     let bebeuAgua = prompt ("Você bebeu água?");
+     let almocou = prompt ("Você já almocou?");
+     let jogo = prompt ("Vai assistir o jogo amanhã?");
+
+     console.log("Você bebeu água?",bebeuAgua);
+     console.log("Você já almoçou?",almocou);
+     console.log("Vai assistir o jogo amanhã?",jogo);
+
+
+     // Questão 3.    /
+
+     // Incompleto /
+
+     let a = 10;
+     let b = 25;
+     let c = undefined;
+
+     c=a;
+     a=b;
+     b=c;
+
+     
+
+     
+  
+
+
+ 
+  
+  
+
+
+
+
