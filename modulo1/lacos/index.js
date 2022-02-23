1192309ca967df4b39c4a69c7eabe03210b5d3f7
Simone Xavier <jerusa.xaviers@gmail.com>
2022-02-23 16:52:58 -0300

    exercicio-lacos

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,40 @@
+//------------------------- Exercícios de interpretação de código---------------------------
+
+/**Questão 1.
+ Executa as instruções encrementando uma variável após cada passagem, o resultado impresso é 10.
+
+ Questão 2.
+
+ a) Todos os números maiores que 18.
+ b) Sim, com a criação de uma nova variável, além disso, o código ficará mais compacto.
+
+ Questão 3.
+ 
+ São adicionadas quatro linhas com os respectivos asteriscos.
+ 
+ */
+
+
+ //---------------------------Exercícios de escrita de código------------------------------------
+
+ 
+ 
+ //Questão 1.
+ 
+const nomeBichinhos =[]
+
+let numeroBichinhos = Number(prompt "Quantos bichinhos?"))
+if (numeroBichinhos ===0){
+  console.log ("que pena")
+} else if(numeroBichinhos > 0){
+  let i = 0
+ while(i < numeroBichinhoos){
+
+nomeBichinhos.push (prompt (`Digite o nome dos seus pets`)
+i++;
+}
+ console.log(nomeBichinhos)
+}
+
+
+// Questão 2.
