5e0959cf7782ce05d10fca1dc0eb9c8d690a6027
Simone Xavier <jerusa.xaviers@gmail.com>
2022-03-23 16:28:56 -0300

    atividade componentes de classe e estados

---
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+
+        <Post
+          nomeUsuario={'tulipa'}
+          fotoUsuario={'https://i.picsum.photos/id/1011/5472/3648.jpg?hmac=Koo9845x2akkVzVFX3xxAc9BCkeGYA9VRVfLE4f0Zzk'}
+          fotoPost={'https://i.picsum.photos/id/100/2500/1656.jpg?hmac=gWyN-7ZB32rkAjMhKXQgdHOIBRHyTSgzuOK6U0vXb1w'}
+        />
+
+        <Post
+          nomeUsuario={'alexandre'}
+          fotoUsuario={'https://i.picsum.photos/id/1023/3955/2094.jpg?hmac=AW_7mARdoPWuI7sr6SG8t-2fScyyewuNscwMWtQRawU'}
+          fotoPost={'https://i.picsum.photos/id/1025/4951/3301.jpg?hmac=_aGh5AtoOChip_iaMo8ZvvytfEojcgqbCH7dzaz-H8Y'}
+        />
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
