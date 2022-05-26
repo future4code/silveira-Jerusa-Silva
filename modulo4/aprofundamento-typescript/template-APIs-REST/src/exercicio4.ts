808d7a023323cdf5a5b234366c593295edd58912
Simone Xavier <jerusa.xaviers@gmail.com>
2022-05-26 16:36:23 -0300

    exercicio aprofundamento typescript

---
+++ modulo4/aprofundamento-typescript/template-APIs-REST/src/exercicio4.ts
@@ -0,0 +1,26 @@
+type pokemon = {
+	name: string,
+  types: string,
+	healthPoints: number
+}
+
+const pokemon1: pokemon = {
+  name: "Charmander",
+  types: "Fire",
+  healthPoints: 28
+}
+
+const pokemon2: pokemon = {
+  name: "Bulbasaur",
+  types: "Grass/Poison",
+  healthPoints: 31
+}
+
+const pokemon3: pokemon = {
+  name: "Squirtle",
+  types: "Water",
+  healthPoints: 35
+}
+
+//rodar no terminal "tsc index.ts", para fazer transpilação.
+//se estiver em uma pasta src, criaria um script.
