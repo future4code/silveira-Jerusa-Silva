2da1c55ef8356b85753b0f76601f976f7c84204e
Simone Xavier <jerusa.xaviers@gmail.com>
2022-07-04 16:38:21 -0300

    atividade arquitetura 1

---
+++ modulo5/arquitetura-de-software-1/template-arquitetura/src/business/UserBusiness.ts
@@ -0,0 +1,97 @@
+import { UserDatabase } from '../data/UserDatabase';
+import { HashManager } from '../services/hashManager';
+import { Authenticator } from '../services/authenticator';
+import { IdGenerator } from '../services/idGenerator';
+import { USER_ROLES } from '../types/User';
+import { User } from '../types/User';
+
+
+const userDatabase = new UserDatabase();
+const hashManager = new HashManager();
+const authenticator = new Authenticator();
+const idGenerator = new IdGenerator();
+
+
+export class UserBusiness{
+    login = async (email:string, password:string) => {
+        if(!email || !password){
+            throw new Error("Campos de e-mail e senha são obrigatórios.");
+        }
+
+        const user = await userDatabase.selectUserByEmail(email);
+
+        if(!user){
+            throw new Error("Usuário não encontrado");
+        }
+
+        const passwordIsCorrect: boolean = await hashManager.compare(password, user.password);
+
+        if(!passwordIsCorrect){
+            throw new Error("Senha inválida");
+        }
+
+        const token = authenticator.generateToken({
+            id: user.id,
+            role: user.role
+        });
+
+        return token;
+}
+
+    signUp = async (name:string, email:string, password:string, role:USER_ROLES) => {
+        if(!name || !email || !password || !role){
+            throw new Error("Missing inputs");
+        }
+
+        if(email.indexOf("@") === -1){
+            throw new Error("E-mail inválido");
+        }
+
+        if(password.length < 6){
+            throw new Error("Senha inválida");
+        }
+
+        if(!(role === USER_ROLES.NORMAL || role === USER_ROLES.ADMIN)){
+            throw new Error("Invalid role");
+        }
+
+        const userId = idGenerator.generateId();
+
+        const hashPassword = await hashManager.hash(password);
+
+        const user = new User(userId, name, email, hashPassword, role);
+
+        await userDatabase.insertUser(user);
+
+        const token = authenticator.generateToken({
+            id: userId,
+            role: role
+        })
+
+        return token;
+    }
+
+    getAllUsers = async (token:string) => {
+        if(!token){
+            throw new Error("Missing token");
+        }
+
+        const users = await userDatabase.getAllUsers();
+
+        return users;
+    }
+
+    deleteUser = async (token:string, id:string) => {
+        if(!token || !id){
+            throw new Error("Missing inputs");
+        }
+
+        const userData = authenticator.getData(token);
+
+        if(userData.role !== USER_ROLES.ADMIN){
+            throw new Error("You are not an admin");
+        }
+
+        await userDatabase.deleteUser(id);
+    }
+}
